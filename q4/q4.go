45126c3739a971b11835b7931569b81d2f8281b7
GuHenri <126472321+GuHenri@users.noreply.github.com>
2023-04-25 10:13:16 -0300

    Update q4.go

--- q4/q4.go
+++ q4/q4.go
@@ -1,9 +1,3 @@
-package q4
-
-import (
-	"fmt"
-)
-
 //Uma loja virtual de roupas recebeu várias listas de produtos vendidos em diferentes dias da semana. O dono da loja
 //deseja analisar as listas para entender melhor o comportamento de suas vendas. Para isso, ele precisa classificar cada
 //lista como em ordem crescente, decrescente ou aleatória, de acordo com o preço dos produtos.
@@ -12,6 +6,11 @@ import (
 //ordem crescente, decrescente ou aleatória. A função deve retornar 1 se a lista estiver em ordem crescente, 2 se a lista
 //estiver em ordem decrescente e 3 se a lista estiver aleatória. A função deve retornar um erro se a lista estiver vazia.
 //Caso a lista possua apenas um elemento, a função deve retornar 3.
+package q4
+
+import (
+	"fmt"
+)
 
 func ClassifyPieces(prices []int) (int, error) {
 
