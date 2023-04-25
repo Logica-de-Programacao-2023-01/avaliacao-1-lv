bed3a86f4759f80f442443cc4852827c088c7af1
GuHenri <126472321+GuHenri@users.noreply.github.com>
2023-04-25 10:03:43 -0300

    Update q5.go

--- q5/q5.go
+++ q5/q5.go
@@ -15,6 +15,26 @@ package q5
 //Ajude Pedro a lidar com esta tarefa fácil.
 
 func ProcessString(s string) string {
-	// Seu código aqui
-	return ""
+	result := ""
+
+	for i := 0; i < len(s); i++ {
+		if strings.Contains("AEIOUaeiou", string(s[i])) {
+			s := strings.ReplaceAll(s, "a", "")
+			s = strings.ReplaceAll(s, "e", "")
+			s = strings.ReplaceAll(s, "i", "")
+			s = strings.ReplaceAll(s, "o", "")
+			s = strings.ReplaceAll(s, "u", "")
+			s = strings.ReplaceAll(s, "A", "")
+			s = strings.ReplaceAll(s, "E", "")
+			s = strings.ReplaceAll(s, "I", "")
+			s = strings.ReplaceAll(s, "O", "")
+			s = strings.ReplaceAll(s, "U", "")
+			s = strings.ToLower(s)
+		}
+		if strings.Contains("BCDFGHJKLMNPQRSTVWXYZbcdfghjklmnpqrstvwxyz", string(s[i])) {
+			output := "." + strings.ToLower(string(s[i]))
+			result += output
+		}
+	}
+	return result
 }
