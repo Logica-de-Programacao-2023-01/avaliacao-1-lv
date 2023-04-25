b48d0810a734730422807e265f0d48b4b652d81f
GuHenri <126472321+GuHenri@users.noreply.github.com>
2023-04-25 09:46:30 -0300

    Update q3.go

--- q3/q3.go
+++ q3/q3.go
@@ -12,7 +12,11 @@ package q3
 //
 //Se M ou N forem iguais ou menores que 0, a função deve retornar um erro.
 
+import "fmt"
+
 func DominoPieces(m, n int) (int, error) {
-	// Seu código aqui
-	return 0, nil
+	if m <= 0 || n <= 0 {
+		return 0, fmt.Errorf("erro")
+	}
+	return (m * n) / 2, nil
 }
